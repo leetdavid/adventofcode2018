4251534e915ff5d5065bc4d84c0334927180e344
David Lee <leetdavidu@gmail.com>
2019-02-25 01:02:03 +0900

    Update day1.js

--- day1.js
+++ day1.js
@@ -1,1 +1,3 @@
-document.getElementsByTagName("pre")[0].textContent.split("\n").reduce((acc, val)=>{ if(val[0]=="+") return +acc+ +val.substring(1); else return +acc- +val.substring(1); },0);
+document.getElementsByTagName("pre")[0].textContent.split("\n").reduce((acc, val)=>{
+  return +acc + +val;
+},0);
