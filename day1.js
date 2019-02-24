44fb6a7f4c9bef4b18a66c4d9390d548063a807b
David Lee <leetdavidu@gmail.com>
2019-02-25 01:08:26 +0900

    Update day1.js

--- day1.js
+++ day1.js
@@ -1,3 +1,3 @@
-document.getElementsByTagName("pre")[0].textContent.split("\n").reduce((acc, val)=>{
+document.getElementsByTagName("pre")[0].textContent.split(/\r?\n/).reduce((acc, val)=>{
   return +acc + +val;
 },0);
